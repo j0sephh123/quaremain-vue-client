81ad21dca3e7989cac2c4ed9189be76403c1a4d6
j0sephh123 <iossif123gg@gmail.com>
2020-05-11 22:12:28 +0300

    settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,15 +1,51 @@
 <template>
   <div class="w-100 text-center mt-3">
-    <button class="btn btn-dark">Reset Database</button>
+    <div class="mb-3">
+      <button class="btn btn-dark">Reset Database</button>
+    </div>
+    <h3>Color themes</h3>
+    <h4>https://bootswatch.com/</h4>
+    <div class="w-75 m-auto">
+      <button
+        @click="themePage(theme)"
+        :class="'font-weight-bold btn btn-block ' + theme"
+        v-for="theme in themes"
+        :key="theme"
+      >{{ theme }}
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    current: "minty",
+    themes: [
+      "minty",
+      "lux",
+      "pulse",
+    ],
+  }),
+  methods: {
+    themePage(theme) {
+      window.open(`https://bootswatch.com/${theme}/`, "_blank")
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.minty {
+  background-color: var(--primary);
+}
+.lux {
+  color: white;
+  background-color: #1a1a1a;
+  border-radius: 0;
+}
+.pulse {
+  background-color: #593196;
+  border-radius: 0;
+}
 </style>
